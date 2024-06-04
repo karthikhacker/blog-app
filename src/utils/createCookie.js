cb07a4b6105f16b5c66955543b9a09e65e67e75a
karthikhacker <karthik.sundararajan85@gmail.com>
2024-06-04 14:34:24 +0530

    modified cookie

--- src/utils/createCookie.js
+++ src/utils/createCookie.js
@@ -1,7 +1,7 @@
 export const createCookie = (res, token) => {
     const options = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-        httpOnly: true,
+        httpOnly: false,
         secure: true
     }
     if (process.env.NODE_ENV === 'production') options.secure = true;
